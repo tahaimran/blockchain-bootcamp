baeb42a8969b4d0d7ef974b7068ef63709087665
tahaimran <tahaimran.24@gmail.com>
2022-02-27 20:49:08 +0500

    Smart Contract Test Done

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity  ^0.5.16;
 
-contract Token{
-    string public name = "My Name"; 
-} 
+contract Token {
+  
+    string public name = "Tak Token";
+    string public symbol = "TK";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+        constructor() public  {
+             totalSupply = 1000000 * (10 ** decimals);
+        }
+}
